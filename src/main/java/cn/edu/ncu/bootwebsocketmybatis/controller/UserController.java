a9af1fb8e3e1c7355de44d4c98cf50a101cd0769
Daniel.ll <1605103328@qq.com>
2018-12-17 15:43:31 +0800

    init

---
+++ src/main/java/cn/edu/ncu/bootwebsocketmybatis/controller/UserController.java
@@ -0,0 +1,10 @@
+package cn.edu.ncu.bootwebsocketmybatis.controller;
+
+/**
+ * @auther: Liu Zedi.
+ * @date: Create in 2018/12/17  15:43
+ * @package: cn.edu.ncu.bootwebsocketmybatis.controller
+ * @project: boot-websocket-mybatis
+ */
+public class UserController {
+}
