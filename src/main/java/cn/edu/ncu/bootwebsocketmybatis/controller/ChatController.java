9659cc7d0e7c52b03791221c18afacaf54a39a23
fuqian <fuqian_918@163.com>
2018-12-24 10:55:47 +0800

    聊天功能完成

--- src/main/java/cn/edu/ncu/bootwebsocketmybatis/controller/ChatController.java
+++ src/main/java/cn/edu/ncu/bootwebsocketmybatis/controller/ChatController.java
@@ -3,19 +3,23 @@ package cn.edu.ncu.bootwebsocketmybatis.controller;
 import cn.edu.ncu.bootwebsocketmybatis.entity.Content;
 import cn.edu.ncu.bootwebsocketmybatis.service.ContentService;
 import com.alibaba.fastjson.JSON;
+import org.apache.commons.io.FileUtils;
+import org.apache.ibatis.annotations.Param;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.websocket.*;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
+import java.io.File;
+import java.io.IOException;
 import java.sql.Timestamp;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 @RestController
@@ -24,24 +28,50 @@ import java.util.concurrent.ConcurrentHashMap;
 @RequestMapping("/chatRoom")
 public class ChatController {
     /**
+     * 使用一个同步Map存储在线的用户
      * 键为userID,值为Session，存储在线用户
      */
     private static Map<String, Session> onlineUsers = new ConcurrentHashMap<>();
     private static Logger logger = LoggerFactory.getLogger(ChatController.class);
+    /**
+     * 存储某个用户的所有好友
+     * 键为某个用户的ID,值为其所有好友的Id集合
+     */
+    private static Map<String,Set<String>> friends = new ConcurrentHashMap<>();
+
+    /**
+     * 存储用户发送图片的路径
+     */
+    @Value("D:/onlineChat/upload/")
+    private String uploadPath;
 
     @Autowired
     private ContentService contentService;
 
+    /**
+     * 好友上线通知其所有的好友,更新状态
+     * @param session
+     * @param sendId
+     */
     @OnOpen
     public void OnOpen(Session session,@PathParam("sendId")String sendId){
         onlineUsers.put(sendId,session);
-        logger.info(sendId + "用户连接了服务器,目前在线人数为：" + onlineUsers.size());
+        for (Map.Entry<String,Set<String>> entry : friends.entrySet())
+            if (entry.getValue().contains(sendId) && onlineUsers.containsKey(entry.getKey()))
+                onlineUsers.get(entry.getKey()).getAsyncRemote().sendText(sendId +" loginIn");
     }
 
+    /**
+     * 用户下线通知其所有好友其下线了,并作状态的转换
+     * @param session
+     * @param sendId
+     */
     @OnClose
     public void OnClose(Session session,@PathParam("sendId")String sendId){
         onlineUsers.remove(sendId);
-        logger.info(sendId + "用户关闭了连接,目前在线人数为：" + onlineUsers.size());
+        for (Map.Entry<String,Set<String>> entry: friends.entrySet())
+            if (entry.getValue().contains(sendId) && onlineUsers.containsKey(entry.getKey()))
+                onlineUsers.get(entry.getKey()).getAsyncRemote().sendText(sendId+" loginOut");
     }
 
     @OnError
@@ -49,6 +79,13 @@ public class ChatController {
         logger.info("发生错误：" + throwable.getMessage());
     }
 
+    /**
+     * 当客户端使用send()方法发送消息给服务器时,触发该事件
+     * 同时服务器进行消息的转发工作,将消息发送给对应的接收者
+     * @param session
+     * @param msg
+     * @param sendId
+     */
     @OnMessage
     public void OnMessage(Session session,String msg,@PathParam("sendId")String sendId){
         Content content = JSON.parseObject(msg, Content.class);
@@ -64,6 +101,12 @@ public class ChatController {
 
     }
 
+    /**
+     * 以Json数据的形式返回 sendId 与 receiveId两个用户的聊天记录
+     * @param sendId
+     * @param receiveId
+     * @return
+     */
     @RequestMapping("/{sendId}/{receiveId}")
     public String getContentRecords(@PathVariable("sendId") String sendId, @PathVariable("receiveId") String receiveId){
         List<Content> list = contentService.getContentRecords(sendId,receiveId);
@@ -71,11 +114,90 @@ public class ChatController {
         return jsonStr;
     }
 
+    /**
+     * 将消息存储至数据库当中
+     * @param json
+     * @return
+     */
     @PostMapping(value = "/send")
     public String send(@RequestBody String json){
         Content content = JSON.parseObject(json,Content.class);
         content.setCreateTime(new Timestamp(new Date().getTime()).toString());
         contentService.insertContentRecord(content);
         return new Timestamp(new Date().getTime()).toString();
+    }
+
+    /**
+     * 返回好友的在线状态 在线还是离线
+     * friend_id中包含了某个用户的所有好友id
+     * 好友的id用" "进行分隔,字符串的最后还包含了请求查询的Id
+     * @param friend_id
+     * @return
+     */
+    @PostMapping(value = "/status")
+    public String getStatus(String friend_id){
+        String json = "";
+        String [] friend = friend_id.split(" ");
+        if (!friends.containsKey(friend[friend.length-1])){
+            Set<String> set = new HashSet<>();
+            for (int i = 0;i<friend.length-1;i++)
+                set.add(friend[i]);
+            friends.put(friend[friend.length-1],set);
+        }
+        for (int i = 0;i<friend.length-1;i++)
+            json += onlineUsers.containsKey(friend[i]) +" ";
+        return json;
+    }
+
+    /**
+     * 将用户发送的图片进行下载到本地
+     * @param file
+     * @param sendId
+     * @param receiveId
+     * @return
+     * @throws IOException
+     */
+    @PostMapping("/upload/picture")
+    public String getUploadPciturePath(@RequestParam("file") MultipartFile file,String sendId,String receiveId)throws IOException {
+        String fileOriginName = file.getOriginalFilename();
+        logger.info(fileOriginName);
+        String fileType = "."+fileOriginName.split("[\\.]")[1];
+        String fileId = generateFileId();
+        if (file!=null){
+            File outFile = new File(uploadPath+"picture/"+sendId+"/"+fileId+fileType);
+            FileUtils.copyInputStreamToFile(file.getInputStream(),outFile);
+        }
+        return "/picture/"+sendId+"/"+fileId+fileType;
+    }
+
+    /**
+     * 将用户发送的语音消息发送到本地
+     * @param audio
+     * @param sendId
+     * @return
+     * @throws IOException
+     */
+    @PostMapping("/upload/audio")
+    public String getUploadAudioPath(@RequestParam("audio")MultipartFile audio,String sendId)throws IOException{
+        logger.info(sendId+"发来了音频");
+        String audioId = generateFileId()+".mp3";
+        int seconds = 0;
+        if (audio!=null){
+            File out = new File(uploadPath +"audio/" +sendId+"/"+audioId);
+            FileUtils.copyInputStreamToFile(audio.getInputStream(),out);
+        }
+        return "/audio/"+sendId +"/"+audioId ;
+    }
+
+    /**
+     * 生成一个8位数字的文件名
+     * @return
+     */
+    private static String generateFileId(){
+        StringBuilder builder = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0;i<=7;i++)
+            builder.append(random.nextInt(10));
+        return builder.toString();
     }
 }
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,6 +1,8 @@
+//初始化好友列表
 function initFriend() {
     initAJAX();
     var userId = document.getElementById("userIf").alt;
+    var friendIds = "";
     xmlHttp.open("POST", "/friend/getAll", true);
     xmlHttp.onreadystatechange = function () {
         if (xmlHttp.readyState == 4) {
@@ -11,19 +13,23 @@ function initFriend() {
                 var chatWindowDivs = "";
                 for (var i in obj) {
                     var friendId = obj[i].friendId;
+                    friendIds += friendId +" ";
                     var userId = obj[i].userId;
                     var image = obj[i].image;
                     var friendName = obj[i].friendName;
-                    listFriend += '<li class="person" data-chat="' + friendId + '">' +
+                    var li_id = friendId+"li";
+                    var tip_id = friendId +"tip";
+                    var status_id = friendId +"status";
+                    listFriend += '<li class="person" id="'+li_id+'" data-chat="' + friendId + '">' +
                         '<img src="' + image + '" alt="' + userId + '" />' +
-                        '<span class="name">' + friendName + '</span>' +
-                        '</li>';
+                        '<span class="name" >' + friendName + '</span><span class="status" id="'+status_id+'">' +
+                        '</span><span class="tip" id='+tip_id+'></span></li>';
                     chatWindowDivs = '<div class="chat" id=' +friendId +' data-chat="' +friendId +'"></div>';
                     $("#write").before(chatWindowDivs);
                 }
                 document.getElementById("people").innerHTML += listFriend;
+                boolOnline(friendIds +" " + userId);
                 init();
-                initChatRecord()
             }
         }
     };
@@ -31,13 +37,18 @@ function initFriend() {
     xmlHttp.send("userId=" + userId);
 }
 
+//进行相关信息的初始化,并连接服务器
 function init() {
-    document.querySelector('.person').classList.add('active');//使person2处于选中状态
-    document.querySelector('.chat').classList.add('active-chat');//选择person2处于正在聊天状态
+    document.querySelector('.person').classList.add('active');//使person处于选中状态
+    document.querySelector('.chat').classList.add('active-chat');//选择person处于正在聊天状态
+    $(".right .top .name").text($(".active .name").text());
+    $(".right .top .status").text($(".active .status").text().substring(1,3))
+
     var friends = {
             list: document.querySelector('ul.people'),//获取好友列表
             all: document.querySelectorAll('.left .person'),//获取所有好友
             name: '' },
+
 
         chat = {
             container: document.querySelector('.container .right'),//获取聊天栏
@@ -45,8 +56,9 @@ function init() {
             person: null,
             name: document.querySelector('.container .right .top .name') };//获取聊天对象名称
 
-    var sendId = $("#userIf").attr("alt");
-    var receiveId = $(".active").attr("data-chat");
+
+    var sendId = $("#userIf").attr("alt"); //获取当前登录用户的userID
+    var receiveId = $(".active").attr("data-chat"); //获取接收者的ID
     initChatRecord(receiveId);
 
     // 监听鼠标点击事件，使点击对象处于active状态
@@ -55,6 +67,12 @@ function init() {
             f.classList.contains('active') || setActiveChat(f);
         });
     });
+
+
+    $(".chat").click(function () {
+        $("#expression").css("visibility","hidden");
+        $("#expressionPackage").css("visibility","hidden");
+    })
 
     function setActiveChat(f) {
         friends.list.querySelector('.active').classList.remove('active');//选择处于active状态的person，并消除刚刚点击的person的active状态
@@ -67,40 +85,117 @@ function init() {
         chat.container.querySelector('[data-chat="' + chat.person + '"]').classList.add('active-chat');//使聊天框中显示选中的person的active-chat
         friends.name = f.querySelector('.name').innerText;//获得聊天对象的name
         chat.name.innerHTML = friends.name;
+        $(".right .top .status").text($(".active .status").text().substring(1,3))
+        f.querySelector(".tip").style.visibility = "hidden";
+        f.querySelector(".tip").innerText ="";
     }
-    
+
+    //返回webSocket对象
     function getWebSocket() {
         var webSocket = new WebSocket("ws://localhost:8080/chatRoom/"+sendId);
         
         webSocket.onopen = function (ev) {
-            alert("发出连接请求")
+            console.log("用户打开了连接");
         }
 
         webSocket.onclose = function (ev) {
-            alert("发生了关闭请求")
+            console.log("用户关闭了连接")
         }
 
         webSocket.onerror = function (ev) {
             alert("发生了错误")
         }
 
-        webSocket.onmessage = function (ev) {
-            alert(ev.data)
-            var obj = JSON.parse(ev.data);
-            alert(obj)
-            for (var i in obj){
-               var sendId = obj[i].sendId;
-               var receiveId = obj[i].receiveId;
-               var createTime = obj[i].createTime;
-               var msg = obj[i].content;
-               alert(sendId +" " + receiveId +" " + createTime+" " + msg)
-           }
+        webSocket.onmessage = function (ev) {  //当有好友发送消息时进行消息的显示
+            if (ev.data.trim().length <= 15){
+                var status_id = ev.data.split(" ")[0] +"status";
+                if (ev.data.split(" ")[1] == "loginIn"){
+                    document.getElementById(status_id).innerText = "[在线]";
+                    if ($(".active").attr("data-chat") == ev.data.split(" ")[0]) $(".right .top .status").text("在线");
+                } else {
+                    document.getElementById(status_id).innerText ="[离线]";
+                    if ($(".active").attr("data-chat") == ev.data.split(" ")[0]) $(".right .top .status").text("离线");
+                }
+            } else {
+                var obj = JSON.parse(ev.data);
+                var sendId = obj.sendId;
+                var receiveId = obj.receiveId;
+                var createTime = obj.createTime.substring(0,19);
+                var content = obj.content;
+                document.getElementById("sound").play();
+                if ($(".active").attr("data-chat") == sendId){  //如果此时，正好打开了与发送消息的好友的窗口,直接显示
+                    var time = document.getElementById($(".active").attr("data-chat")).getElementsByClassName("conversation-start");
+                    var lastTime;
+                    if (time.length == 0) lastTime ="2010-12-22 11:08:29";
+                    else  lastTime = time[time.length-1].innerText;
+                    if (judgeShowTime(createTime,lastTime)){
+                        if (boolToday(createTime))
+                            $(".active-chat").append('<div class=\"conversation-start\"><span>'+createTime.split(" ")[1]+'</span></div>');
+                        else
+                            $(".active-chat").append('<div class=\"conversation-start\"><span>'+createTime+'</span></div>');
+                    }
+                    $(".active-chat").append("<div class='bubble you'>"+content+"</div>")
+                } else { //如果此时没有打开与发送消息好友的窗口,则给出提醒
+                    var li_id = sendId +"li";
+                    var person = document.getElementById(li_id);
+                    var tip_id = sendId +"tip";
+                    var tip = document.getElementById(tip_id);
+                    var status_id = sendId +"status";
+                    var status = document.getElementById(status_id).innerText;
+                    var unread;
+                    if (tip.innerText == "") unread = 1;
+                    else unread = parseInt(tip.innerText)+1;
+                    var img_url = person.getElementsByTagName("img")[0].getAttribute("src");
+                    var friendName = person.getElementsByTagName("span")[0].innerText;
+                    $("#people").prepend('<li class="person" data-chat="'+sendId +'" id="'+li_id+'">' +
+                        '<img src="'+img_url+'" alt ="'+receiveId+'"/><span class="name">' +friendName +'</span><span class="status" id="'+status_id+'">'+status+'</span><span class="tip" ' +
+                        'id="'+tip_id+'" style="visibility: visible">'+unread+'</span></li> ')
+                    person.parentNode.removeChild(person);
+                    friends = {
+                        list: document.querySelector('ul.people'),//获取好友列表
+                        all: document.querySelectorAll('.left .person'),//获取所有好友
+                        name: '' };
+                    chat = {
+                        container: document.querySelector('.container .right'),//获取聊天栏
+                        current: null,
+                        person: null,
+                        name: document.querySelector('.container .right .top .name') };//获取聊天对象名称
+                    friends.all.forEach(function (f) {
+                        f.addEventListener('mousedown', function () {
+                            f.classList.contains('active') || setActiveChat(f);
+                        });
+                    });
+                }
+            }
         }
 
         return webSocket;
     }
 
+    //用户与服务器进行连接
     var webSocket = getWebSocket();
+
+    //用户发送自己的图片
+    $("#inputImage").change(function () {
+        var file = document.getElementById("inputImage").files[0];
+        if (file == null) return;
+        var formData = new FormData();
+        formData.append("file",file)
+        formData.append("sendId",sendId);
+        formData.append("receiveId",receiveId);
+        $.ajax({
+            url: "/chatRoom/upload/picture",
+            type: "POST",
+            data: formData,
+            contentType: false,
+            processData: false,
+            success: function (ev) {
+                $(".div-textarea").html("<img class='userImg chatPicture' src=" + ev + ">");
+                $("#chat-fasong").click();
+            }
+        })
+    })
+
 
     //发送功能
     $("#chat-fasong").click(function () {
@@ -110,9 +205,8 @@ function init() {
             var content = {
                 "sendId": $("#userIf").attr("alt"),
                 "receiveId":$(".active").attr("data-chat"),
-                "content":$(".div-textarea").text()
+                "content":$(".div-textarea").html()
             }
-            alert(JSON.stringify(content));
             webSocket.send(JSON.stringify(content));
             $.ajax({
                 url:"/chatRoom/send",
@@ -121,7 +215,9 @@ function init() {
                 contentType : "application/json;charset=utf-8",
                 success:function (ev) {
                     var time = document.getElementById($(".active").attr("data-chat")).getElementsByClassName("conversation-start");
-                    var lastTime = time[time.length-1].innerText;
+                    var lastTime;
+                    if (time.length == 0) lastTime ="2010-12-22 11:08:29";
+                    else  lastTime = time[time.length-1].innerText;
                     var currentTime = ev.trim().substring(0,19);
                     if (judgeShowTime(currentTime,lastTime))
                         if (boolToday(currentTime)){
@@ -136,8 +232,83 @@ function init() {
         }else {
             alert("发送内容不能为空");
         }
+
     });
+
+    //语音发送功能
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia){
+        navigator.mediaDevices.getUserMedia({
+            "audio":true
+        })
+            .then(function (value) {
+                var mediaRecorder = new MediaRecorder(value);
+                var start,end;
+                var chunks = [];
+
+                $("#chat-audio").mousedown(function () {
+                    start = new Date();
+                    mediaRecorder.start();
+                    $(this).css({
+                        "width":"44px",
+                        "height":"40px",
+                        "top":"2px",
+                        "left":"374px"
+                    })
+                })
+
+                mediaRecorder.ondataavailable = function(ev){
+                    chunks.push(ev.data)
+                }
+
+                $("#chat-audio").mouseup(function () {
+                    end = new Date();
+                    mediaRecorder.stop();
+                    $(this).css({
+                        "width":"22",
+                        "height":"20px",
+                        "left": "384px",
+                        "top": "12px"
+                    })
+                })
+
+                mediaRecorder.onstop = function (ev) {
+                    if (end.getTime() - start.getTime() <1000){
+                        alert("录音时间过短");
+                    }else {
+                        var blob = new Blob(chunks,{"type":'audio/mp3;codecs=opus'})
+                        var formData = new FormData();
+                        formData.append("audio",blob);
+                        formData.append("sendId",sendId);
+                        $.ajax({
+                            url: "/chatRoom/upload/audio",
+                            type: "POST",
+                            data: formData,
+                            contentType: false,
+                            processData: false,
+                            success: function (ev) {
+                                var second = Math.round((end.getTime() -start.getTime())/1000);
+                                $(".div-textarea").html('<img class="soundImg" onclick="sound(this)" src="img/soundMsg.png" alt="'+ev+'">'+'<span style="margin-left: 10px">'+second+"</span>");
+                                $("#chat-fasong").click();
+                            }
+                        })
+                        chunks = [];
+                    }
+                }
+            })
+            .catch(function (reason) {
+                alert("请打开录音权限")
+            })
+    } 
 }
+
+//播放语音消息
+function sound(img) {
+    var soundURL = img.getAttribute("alt");
+    $("#msgAudio").attr("src",soundURL);
+    document.getElementById("msgAudio").play();
+}
+
+
 //点击哪位好友,生成其聊天记录
 function initChatRecord(contactId) {
     var userId = $("#userIf").attr("alt");
@@ -152,7 +323,10 @@ function initChatRecord(contactId) {
             var sendId = obj[i].sendId;
             var receiveId = obj[i].receiveId;
             if (show){
-                openedChatWindow.innerHTML += '<div class=\"conversation-start\"><span>'+sendTime+'</span></div>';
+                if (boolToday(sendTime))
+                    openedChatWindow.innerHTML += '<div class=\"conversation-start\"><span>'+sendTime.split(" ")[1]+'</span></div>';
+                else
+                    openedChatWindow.innerHTML += '<div class=\"conversation-start\"><span>'+sendTime+'</span></div>';
                 show = false;
             }
             var time = document.getElementById(contactId).getElementsByClassName("conversation-start");
@@ -162,7 +336,6 @@ function initChatRecord(contactId) {
                     openedChatWindow.innerHTML += '<div class=\"conversation-start\"><span>'+sendTime.split(" ")[1]+'</span></div>';
                 else
                     openedChatWindow.innerHTML += '<div class=\"conversation-start\"><span>'+sendTime+'</span></div>';
-
             if (userId == sendId)
                 openedChatWindow.innerHTML += '<div class=\"bubble me\">' + content +'</div>';
             else
@@ -176,15 +349,28 @@ function judgeShowTime(currentTime,pastTime) {
     var reg = new RegExp("[^0-9]");
     var current = currentTime.trim().split(reg);
     var past = pastTime.trim().split(reg)
-    var current_year = parseInt(current[0]);var past_year = parseInt(past[0]);
-    var current_month = parseInt(current[1]);var past_month = parseInt(past[1]);
-    var current_day = parseInt(current[2]);var past_day = parseInt(past[2]);
-    var current_hour = parseInt(current[3]);var past_hour = parseInt(past[3]);
-    var current_minute = parseInt(current[4]);var past_minute = parseInt(past[4]);
-    var current_second = parseInt(current[5]);var past_second = parseInt(past[5]);
-    if (current_year>past_year || current_month>past_month || current_day > past_day ||current_hour>past_hour) return true;
-    else if ((current_minute*60+current_second) >= (past_minute*60+past_second + 180)) return true;
-    else return false;
+    var current_year = parseInt(current[0]);var current_month = parseInt(current[1]);
+    var current_day = parseInt(current[2]); var current_hour = parseInt(current[3]);
+    var current_minute = parseInt(current[4]);var current_second = parseInt(current[5]);
+
+    if (pastTime.length == 19){
+       var past_year = parseInt(past[0]);
+       var past_month = parseInt(past[1]);
+       var past_day = parseInt(past[2]);
+       var past_hour = parseInt(past[3]);
+       var past_minute = parseInt(past[4]);
+       var past_second = parseInt(past[5]);
+       if (current_year>past_year || current_month>past_month || current_day > past_day ||current_hour>past_hour) return true;
+       else if ((current_minute*60+current_second) >= (past_minute*60+past_second + 180)) return true;
+       else return false;
+    }else {
+        var past_hour = parseInt(past[0]);
+        var past_minute = parseInt(past[1]);
+        var past_second = parseInt(past[2]);
+        if (current_hour>past_hour)  return true;
+        else if ((current_minute*60+current_second) >= (past_minute*60+past_second + 180)) return true;
+        else return false;
+    }
 }
 
 //判断是否为当天,如果是当天的话,返回false,不显示年月日
@@ -195,4 +381,27 @@ function boolToday(time) {
     return (time_array[0] == date.getFullYear() && time_array[1] == date.getMonth()+1 && time_array[2] == date.getDate());
 }
 
+//返回好友的在线情况
+function boolOnline(friend_id) {
+    $.ajax({
+        url: "/chatRoom/status",
+        type:"POST",
+        data: "friend_id="+friend_id,
+        data_type:"json",
+        async: false,
+        success:function(ev){
+            var friendIds = friend_id.trim().split(" ");
+            var status = ev.split(" ");
+            for (var i = 0;i<friendIds.length-2;i++){
+                var status_id = friendIds[i].trim() +"status";
+                if (status[i] == "true"){
+                    document.getElementById(status_id).innerText = "[在线]";
+                }
+                 else{
+                    document.getElementById(status_id).innerText = "[离线]";
+                }
+            }
+        }
+    })
+}
 
